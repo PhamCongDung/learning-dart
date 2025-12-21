5c9bdd8998d3858b24f07626aa06add8518fa420
DungPham <phamcongdung92@gmail.com>
2025-12-21 21:34:51 +0700

    feat : add Counter Stream to project

--- MiniProject/User_Manangement/userManagement.dart
+++ MiniProject/User_Manangement/userManagement.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 class User {
   String id;
   String name;
@@ -29,6 +31,7 @@ abstract class Repository<T> {
 
 class UserRepository implements Repository<User> {
   final List<User> _storage = [];
+  int get count => _storage.length;
   @override
   void add(User user) {
     if (_storage.any((x) => x.id == user.id)) {
@@ -54,4 +57,41 @@ extension UserListX on List<User> {
     final index = this.indexWhere((x) => x.id == id);
     return index == -1 ? null : this[index];
   }
+}
+
+class CounterStream {
+  final _controller = StreamController<int>.broadcast();
+  Stream<int> get stream => _controller.stream;
+  void emit(int count) {
+    _controller.add(count);
+  }
+
+  void dispose() {
+    _controller.close();
+  }
+}
+
+void main() {
+  final counter = CounterStream();
+  counter.stream.listen((x) => print('Current user count : $x'));
+  var repo = UserRepository();
+  repo.add(User("1", "Dung", 33));
+  counter.emit(repo.count);
+  repo.add(User("2", "Thanh", 32));
+  counter.emit(repo.count);
+  try {
+    repo.add(User("1", "Leo", 2));
+  } on DuplicateUserException catch (e) {
+    print("Error : $e");
+  }
+  var user_found = repo.findById("1");
+  print("Found user : $user_found");
+  try {
+    repo.findById("100");
+  } on UserNotFoundException catch (e) {
+    print("Error : User not found : 100");
+  }
+  var all_user = repo.getAll();
+  print("All user : \n $all_user");
+  counter.dispose();
 }
